25c2796ecf13309357e13bf2f63ae8696ee51310
muhammedulas <muhammedulas3333@gmail.com>
2021-06-12 15:22:53 +0300

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,25 +16,28 @@ import { _dashboardComponent } from './components/_dashboard/_dashboard.componen
 import { _tasksComponent } from './components/_tasks/_tasks.component';
 import { _teamsComponent } from './components/_teams/_teams.component';
 import { _todosComponent } from './components/_todos/_todos.component';
+import { AuthGuard } from './services/auth.guard';
 
 const routes: Routes = [
   {
-    path: '', component: MainComponent, children: [
-      { path: '', component: LandingComponent },
-      { path: 'admin', component: AdminComponent, children:[
-        {path:'teams', component:TeamsComponent},
-        {path:'company', component:CompanyComponent},
-        {path:'departments', component:DepartmentsComponent},
-        {path:'users', component:UsersComponent},
-        {path:'tasks', component:TasksComponent},
-        {path:'announcements', component:AnnouncementsComponent}
-      ]},
-      {path:'todos', component:_todosComponent},
-      {path:'company', component:_companyComponent},
-      {path:'dashboard', component:_dashboardComponent},
-      {path:'tasks', component:_tasksComponent},
-      {path:'teams', component: _teamsComponent},
-      {path:'profile', component:ProfileComponent}
+    path: '', component: MainComponent, canActivate: [AuthGuard], children: [
+      { path: '', component: LandingComponent, canActivate: [AuthGuard] },
+      {
+        path: 'admin', component: AdminComponent, canActivate: [AuthGuard], children: [
+          { path: 'teams', component: TeamsComponent },
+          { path: 'company', component: CompanyComponent },
+          { path: 'departments', component: DepartmentsComponent },
+          { path: 'users', component: UsersComponent },
+          { path: 'tasks', component: TasksComponent },
+          { path: 'announcements', component: AnnouncementsComponent }
+        ]
+      },
+      { path: 'todos', component: _todosComponent, canActivate: [AuthGuard] },
+      { path: 'company', component: _companyComponent, canActivate: [AuthGuard] },
+      { path: 'dashboard', component: _dashboardComponent, canActivate: [AuthGuard] },
+      { path: 'tasks', component: _tasksComponent, canActivate: [AuthGuard] },
+      { path: 'teams', component: _teamsComponent, canActivate: [AuthGuard] },
+      { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] }
     ]
   },
   { path: 'login', component: LoginComponent }
--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -2,15 +2,38 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
+import { tokenResponse } from '../models/tokenResponse';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+  private session: tokenResponse = new tokenResponse();
+  private date: Date = new Date();
+  constructor(
+    private router: Router,
+    private auth:AuthService
+  ) {
+    this.session = JSON.parse(localStorage.getItem('session'))
+  }
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    if (this.session || (Date.parse(this.date.toString()) < Date.parse(this.session['.expires']))) {
+      if (route.routeConfig.path == 'admin') {
+        if (this.session.isAdmin == 'true') return true
+        else {
+          this.auth.logout()
+          return false
+        }
+      }
+      else return true
+    }
+    else {
+      this.auth.logout()
+      return false
+    }
   }
-  
+
 }
